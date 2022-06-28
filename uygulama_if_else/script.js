97b764e9153ed2c3b469d96e668d8a93d1a99831
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-06-28 08:53:09 +0200

    uygulama if else

---
+++ uygulama_if_else/script.js
@@ -0,0 +1,10 @@
+// 1- Bir sayının 10-50 arasında olup olmadığını kontrol ediniz.
+
+const sayi = 0;
+
+if (sayi < 10 || sayi > 50) {
+
+    console.log("Girilen sayı 10 ile 50 arasında değil");
+} else {
+    console.log("Girdiğiniz sayı 10 ile arasında");
+}
