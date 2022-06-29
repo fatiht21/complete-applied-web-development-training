34364299ff2a596a1c3ee6384ceda64176061575
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-06-29 12:34:10 +0200

    uygulama_string

--- uygulama_string/script.js
+++ uygulama_string/script.js
@@ -1,9 +1,10 @@
 let url = "https://www.sadikturan.com/";
-let kursAdi = "Komple Web Geliştirme Kursu";
+let kursAdi = "Komple Web Gelistirme Kursu";
 
 // 1- url kaç karakterlidir?
 
-        sonuc = url.length;
+     let sonuc = url.length;
+
 // 2- kursAdi kaç kelimeden oluşmaktadır?
 
         sonuc = kursAdi.split(" ").length;
@@ -11,5 +12,28 @@ let kursAdi = "Komple Web Geliştirme Kursu";
 // 3- url https ile mi başlıyor?
 
         sonuc = url.startsWith("https");
+
+        if (sonuc) {
+                console.log("Evet başlıyor");
+        }
+
+// 4- kursAdi içerisinde Eğitimi kelimesi var mı?
+        
+        sonuc = kursAdi.includes("Eğitimi");
+
+        if (kursAdi.indexOf("Eğitimi") !== -1) {
+                console.log("Evet içeriyor");
+        } else {
+                console.log("Hayır içermiyor")
+        }
+
+// 5- url ve kursAdi değişkenlerini kullanarak aşağıdaki string bilgiyi oluşturunuz.
+
+// https://www.sadikturan.com/komple-web-gelistirme-kursu
+
+kursAdi = kursAdi.replaceAll(" ", "-").toLocaleLowerCase();
+
+sonuc = `${url}${kursAdi}`
+
 
 console.log(sonuc)
