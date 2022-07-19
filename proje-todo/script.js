daf4de13516989075056c8b248c61522054bea6d
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-07-19 13:09:57 +0200

    completed items marked

--- proje-todo/script.js
+++ proje-todo/script.js
@@ -1,8 +1,8 @@
 let gorevListesi = [
-  { id: 1, gorevAdi: "Görev 1" },
-  { id: 2, gorevAdi: "Görev 2" },
-  { id: 3, gorevAdi: "Görev 3" },
-  { id: 4, gorevAdi: "Görev 4" },
+  { id: 1, gorevAdi: "Görev 1", status: "completed" },
+  { id: 2, gorevAdi: "Görev 2", status: "pending" },
+  { id: 3, gorevAdi: "Görev 3", status: "completed" },
+  { id: 4, gorevAdi: "Görev 4", status: "pending" },
 ];
 
 let editId;
@@ -19,11 +19,12 @@ function displayTasks() {
     ul.innerHTML = "<p class='p-3 m-3'>Görev Listeniz Boş!</p>";
   } else {
     for (let gorev of gorevListesi) {
+      let completed = gorev.status == "completed" ? "checked" : "";
       let li = `
   <li class="task list-group-item">
         <div class="form-check">
-          <input type="checkbox" id="${gorev.id}" class="form-check-input" />
-          <label for="${gorev.id}" class="form-check-label">${gorev.gorevAdi}</label>
+          <input type="checkbox" onclick="updateStatus(this)" id="${gorev.id}" class="form-check-input" ${completed} />
+          <label for="${gorev.id}" class="form-check-label ${completed}">${gorev.gorevAdi}</label>
         </div>
         <div class="dropdown">
           <button class="btn btn-link dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
@@ -99,3 +100,22 @@ btnClear.addEventListener("click", function () {
   gorevListesi.splice(0, gorevListesi.length);
   displayTasks();
 });
+
+function updateStatus(selectedTask) {
+  let label = selectedTask.nextElementSibling;
+  let status;
+
+  if (selectedTask.checked) {
+    label.classList.add("checked");
+    status = "completed";
+  } else {
+    label.classList.remove("checked");
+    status = "pending";
+  }
+
+  for (let gorev of gorevListesi) {
+    if (gorev.id == selectedTask.id) {
+      gorev.durum = durum;
+    }
+  }
+}
