e1d2761721d81e0760c1f4542bc7898d0f3fe7bf
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-07-23 17:14:03 +0200

    control of answers options

--- proje quiz-app/script.js
+++ proje quiz-app/script.js
@@ -51,16 +51,21 @@ const quiz = new Quiz(sorular);
 document.querySelector(".btn_start").addEventListener("click", function () {
   document.querySelector(".quiz_box").classList.add("active");
   soruGoster(quiz.soruGetir());
+  document.querySelector(".next_btn").classList.remove("show");
 });
 
 document.querySelector(".next_btn").addEventListener("click", function () {
   if (quiz.sorular.length > quiz.soruIndex + 1) {
-    quiz.soruIndex++;
+    quiz.soruIndex += 1;
     soruGoster(quiz.soruGetir());
+    document.querySelector(".next_btn").classList.remove("show");
   } else {
     console.log("Quiz bitti!");
   }
 });
+const option_list = document.querySelector(".option_list");
+const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
+const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>';
 
 function soruGoster(soru) {
   let question = `<span>${soru.soruMetni}</span>`;
@@ -72,7 +77,37 @@ function soruGoster(soru) {
         <span><b>${cevap}</b>: ${soru.cevapSecenekleri[cevap]}</span>
       </div>
     `;
-    document.querySelector(".question_text").innerHTML = question;
-    document.querySelector(".option_list").innerHTML = options;
   }
+
+  document.querySelector(".question_text").innerHTML = question;
+  option_list.innerHTML = options;
+
+  const option = option_list.querySelectorAll(".option");
+
+  for (let opt of option) {
+    opt.setAttribute("onclick", "optionSelected(this)");
+  }
+
+  // for (let opt of option) {
+  //   opt.setAttribute("onclick", "optionSelected(this");
+  // }
+}
+
+function optionSelected(option) {
+  let cevap = option.querySelector("span b").textContent;
+  let soru = quiz.soruGetir();
+
+  if (soru.cevabiKontrolEt(cevap)) {
+    option.classList.add("correct");
+    option.insertAdjacentHTML("beforeend", correctIcon);
+  } else {
+    option.classList.add("incorrect");
+    option.insertAdjacentHTML("beforeend", incorrectIcon);
+  }
+
+  for (let i = 0; i < option_list.children.length; i++) {
+    option_list.children[i].classList.add("disabled");
+  }
+
+  document.querySelector(".next_btn").classList.add("show");
 }
